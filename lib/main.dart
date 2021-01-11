a69b6c3f54949a4eadd0734cdbe80de99e1504d0
phuonghnl <hnlphuong.1902@gmail.com>
2021-01-11 21:48:17 +0700

    create splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:pet_rescue_mobile/src/asset.dart';
+import 'package:pet_rescue_mobile/src/style.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import 'views/login_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  new MaterialApp(
+    home: new MyApp(),
+  )
+);
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyApp createState() => new _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rescue Them',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return new SplashScreen(
+      seconds: 7,
+      navigateAfterSeconds: LoginPage(),
+      image: new Image.asset(
+        app_logo,
       ),
-      home: LoginPage(),
+      photoSize: 120,
+      backgroundColor: common,
+      loaderColor: Colors.red,
     );
   }
 }
+
+// class _MyApp extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Rescue Them',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginPage(),
+//     );
+//   }
+// }
